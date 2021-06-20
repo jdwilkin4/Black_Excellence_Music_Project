c15e1c6884c0c3d16e7e412b78b93bbeceff73b3
Jessica Wilkins <jwilkin4@hotmail.com>
2021-06-20 01:22:37 -0700

    removing create react app jsx in app.js

---
+++ artists-files/contemporary-classical-composers.js
@@ -0,0 +1,15 @@
+export const classicalComposers = [
+    {
+        name: 'Adams, Harrison Leslie',
+        dates: '(b. 1932)',
+        tag: 'Composer, Pianist & Professor',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Adams.html'
+    },
+    {
+        name: 'Akpabot, Samuel Ekpe',
+        dates: '(1932-2000)',
+        tag: 'Nigerian Composer, Professor and Author',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Akpabot.html'
+    }
+
+]
