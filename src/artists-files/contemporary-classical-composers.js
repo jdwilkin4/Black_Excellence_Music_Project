68ff31d6a7ad87534e83d7c52ca52e4727f3eba7
Jessica Wilkins <jwilkin4@hotmail.com>
2021-06-20 04:59:26 -0700

    adding classical contemporary composers

--- src/artists-files/contemporary-classical-composers.js
+++ src/artists-files/contemporary-classical-composers.js
@@ -8,7 +8,208 @@ const contemporaryClassicalComposers = [
         name: 'Akpabot, Samuel Ekpe (1932-2000)',
         tag: 'Nigerian Composer, Professor and Author',
         bio: 'http://chevalierdesaintgeorges.homestead.com/Akpabot.html'
-    }
+    },
+    {
+        name: 'Alberga, Eleanor (b. 1949)',
+        tag: 'Jamaican Composer & Pianist',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Alberga.html'
+    },
+    {
+        name: 'Bonds, Margaret Allison (1913-1972)',
+        tag: 'African American Composer, Pianist & Musical Director ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Bonds.html'
+    },
+    {
+        name: 'Burleigh, Henry "Harry" T. (1866-1949) ',
+        tag: 'African American Composer, Arranger & Baritone ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Burleigh.html'
+    },
+    {
+        name: 'Coleridge-Taylor, Samuel (1875-1912)',
+        tag: 'Afro-British Composer, Conductor & Professor',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Song.html'
+    },
+    {
+        name: 'Cunningham, Arthur (1928-1997)',
+        tag: 'African American Composer & Pianist ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Cunningham.html'
+    },
+    {
+        name: 'Dawson, William Levi (1899-1990)',
+        tag: 'African American Composer & Professor',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Dawson.html'
+    },
+    {
+        name: 'Dédé, Edmund (1827-1903)',
+        tag: 'African American Composer, Violinist & Conductor ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Dede.html'
+    },
+    {
+        name: 'Dett, R. Nathaniel (1882-1943) ',
+        tag: 'African American Composer, Pianist & Choral Director',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Dett.html'
+    },
+    {
+        name: 'Elie, Justin (1883-1931)',
+        tag: 'Haitian Composer & Pianist ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Elie.html'
+    },
+    {
+        name: 'Euba, Akin (1935 - 2020)',
+        tag: 'Nigerian Composer, Professor & Author ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Euba.html'
+    },
+    {
+        name: 'Hailstork, Adolphus C. (b. 1941) ',
+        tag: 'African American Composer & Professor ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Hailstork.html'
+    },
+    {
+        name: 'Holland, Justin  (1819-1887) ',
+        tag: 'African American Composer, Guitarist & Teacher',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Holland.html'
+    },
+    {
+        name: 'Kay, Ulysses (1917-1995) ',
+        tag: 'African American Composer, Conductor & Professor ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Kay.html'
+    },
+    {
+        name: 'Khumalo, J. S. Mzilikazi (b. 1932) ',
+        tag: 'South African Composer, Arranger & Choral Director ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Khumalo.html'
+    },
+    {
+        name: 'Lamothe, Ludovic  (1882-1953)  ',
+        tag: 'Haitian Composer & Pianist ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Lamothe.html'
+    },
+    {
+        name: 'León, Tania Justina (b. 1943) ',
+        tag: 'Afro-Cuban Composer, Conductor & Professor ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Leon.html'
+    },
+    {
+        name: 'Moerane, Michael Mosoeu (1904-1980)',
+        tag: 'South African-Basotho Composer, Teacher, Pianist & Conductor',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Moerane.html'
+    },
+    {
+        name: 'Perkinson, Coleridge-Taylor (1932-2004) ',
+        tag: 'African American Composer & Conductor ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Perkinson.html'
+    },
+    {
+        name: 'Pradel, Alain Pierre (b. 1949) ',
+        tag: 'Guadeloupean Composer & Pianist ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Pradel.html'
+    },
+    {
+        name: 'Price, Florence Beatrice (1887-1953) ',
+        tag: 'African American Composer, Arranger & Teacher ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Price.html'
+    },
+    {
+        name: 'Racine, Julio (1945 - 2020) ',
+        tag: 'Haitian Composer, Arranger & Flutist ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Racine.html'
+    },
+    {
+        name: 'Saint-Georges, Le Chevalier de (1745-1799)',
+        tag: 'Afro-French Composer, Violinist & Conductor ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Page1.html'
+    },
+    {
+        name: 'Sancho, Ignatius (1729-1780)',
+        tag: 'From African Slave to Composer & Author ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Sancho.html'
+    },
+    {
+        name: 'Smith, Hale (1925-2009)',
+        tag: 'African American Composer, Pianist & Professor',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Smith.html'
+    },
+    {
+        name: 'Smith, Irene Britton (1907-1999)',
+        tag: 'African American Composer ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Britton.html'
+    },
+    {
+        name: 'Sowande, Fela (1905-1987)',
+        tag: 'Nigerian Composer, Organist & Professor ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Sowande.html'
+    },
+    {
+        name: 'Still, William Grant (1895-1978)',
+        tag: 'African American Composer, Arranger, Conductor & Oboist',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Still.html'
+    },
+    {
+        name: 'Walker, George  (1922 - 2018)',
+        tag: 'African American Composer & Pianist ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Walker.html'
+    },
+    {
+        name: 'Williams, Julius Penson (b. 1954)',
+        tag: 'African American Composer, Conductor & Professor ',
+        bio: 'http://chevalierdesaintgeorges.homestead.com/Williams.html'
+    },
+    {
+        name: ' ',
+        tag: '',
+        bio: ''
+    },
+    {
+        name: ' ',
+        tag: '',
+        bio: ''
+    },
+    {
+        name: ' ',
+        tag: '',
+        bio: ''
+    },
+    {
+        name: ' ',
+        tag: '',
+        bio: ''
+    },
+    {
+        name: ' ',
+        tag: '',
+        bio: ''
+    },
+    {
+        name: ' ',
+        tag: '',
+        bio: ''
+    },
+    {
+        name: ' ',
+        tag: '',
+        bio: ''
+    },
+    {
+        name: ' ',
+        tag: '',
+        bio: ''
+    },
+    {
+        name: ' ',
+        tag: '',
+        bio: ''
+    },
+    {
+        name: ' ',
+        tag: '',
+        bio: ''
+    },
+    {
+        name: ' ',
+        tag: '',
+        bio: ''
+    },
+
 
 ]
 export default contemporaryClassicalComposers
