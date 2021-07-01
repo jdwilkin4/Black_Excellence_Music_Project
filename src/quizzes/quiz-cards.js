467b5214c03eca3de67a2bfbade8a22a7fcc40f8
Jessica Wilkins <jwilkin4@hotmail.com>
2021-07-01 03:02:27 -0700

    adding artists to quizzes page

--- src/quizzes/quiz-cards.js
+++ src/quizzes/quiz-cards.js
@@ -1,6 +1,27 @@
 const quizCards = [
     {
-
+        name: 'Louis Armstrong',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Louis_Armstrong_restored.jpg/220px-Louis_Armstrong_restored.jpg',
+        alt: 'Louis Armstrong',
+        bio: 'https://kids.britannica.com/kids/article/Louis-Armstrong/352783'
+    },
+    {
+        name: 'Ella Fitzgerald',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Ella_Fitzgerald_%28Gottlieb_02871%29.jpg/220px-Ella_Fitzgerald_%28Gottlieb_02871%29.jpg',
+        alt: 'Ella Fitzgerald',
+        bio: 'https://kids.britannica.com/kids/article/Ella-Fitzgerald/353131'
+    },
+    {
+        name: 'Samuel Coleridge-Taylor',
+        image: 'https://www.blackpast.org/wp-content/uploads/prodimages/files/blackpast_images/Coleridge-Taylor_Samuel.jpg',
+        alt: 'Samuel Coleridge-Taylor',
+        bio: 'https://wiki.kidzsearch.com/wiki/Samuel_Coleridge-Taylor'
+    },
+    {
+        name: 'Nina Simone',
+        image: 'https://static01.nyt.com/images/2015/06/21/arts/21SIMONE/21SIMONE-superJumbo.jpg',
+        alt: 'Nina Simone',
+        bio: 'https://wiki.kidzsearch.com/wiki/Nina_Simone'
     }
 ]
 export default quizCards;
