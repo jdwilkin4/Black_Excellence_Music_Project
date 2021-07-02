c978e38c6a59e2a450f4e065d7f714ee9430e5a8
Jessica Wilkins <jwilkin4@hotmail.com>
2021-07-02 00:42:17 -0700

    working on styles for quizzes page

--- src/quizzes/QuizzesPage.jsx
+++ src/quizzes/QuizzesPage.jsx
@@ -3,31 +3,27 @@ const QuizzesPage = () => {
     return (
         <div>
             <h1 className="text-5xl text-center my-8 ">Quizzes</h1>
-
-
             <div className="flex flex-row flex-wrap justify-center items-center my-2">
                 {quizCards.map((artist, index) => (
                     <div key={index}>
-                        <div className="mx-4 border-r border-l border-b-8 border-t-8 border-purple-700 rounded-lg p-8 flex flex-col justify-between leading-normal">
-                            <div className=" text-center mb-2">
-                                <h2 className="text-black font-bold text-xl mb-2">{artist.name}</h2>
+                        <div className="mx-4 mb-4 border-r border-l border-b-8 border-t-8 border-purple-700 rounded-lg p-8  justify-between leading-normal">
+                            <div className="w-56 text-center mb-2">
+                                <h2 className="text-black font-bold text-lg mb-2">{artist.name}</h2>
                                 <div className="photo-wrapper p-2">
                                     <img className="w-40 h-40 rounded-full mx-auto" src={artist.image} alt={artist.alt} />
                                 </div>
                             </div>
-                            <div className="text-center my-4">
+                            <div className="text-center mt-4 mb-8">
                                 <a rel="noreferrer" target="_blank" href={artist.bio} className="w-1/2 px-4 py-3 text-white bg-purple-700  rounded-lg font-bold text-sm">{artist.name} Bio</a>
                             </div>
-                            <div className="text-center my-2">
-                                <a href='/' className="w-1/2 px-4 py-3 text-white bg-green-700  rounded-lg font-bold text-sm">Take quiz</a>
+                            <div className="text-center">
+                                <a href={artist.quiz} className="w-1/2 px-4 py-3 text-white bg-green-700  rounded-lg font-bold text-sm">Take quiz</a>
                             </div>
                         </div>
                     </div>
                 ))}
-
             </div>
         </div>
-
     )
 
 }
--- src/quizzes/quiz-cards.js
+++ src/quizzes/quiz-cards.js
@@ -3,49 +3,57 @@ const quizCards = [
         name: 'Louis Armstrong',
         image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Louis_Armstrong_restored.jpg/220px-Louis_Armstrong_restored.jpg',
         alt: 'Louis Armstrong',
-        bio: 'https://kids.britannica.com/kids/article/Louis-Armstrong/352783'
+        bio: 'https://kids.britannica.com/kids/article/Louis-Armstrong/352783',
+        quiz: '/armstrongquiz'
     },
     {
         name: 'Ella Fitzgerald',
         image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Ella_Fitzgerald_%28Gottlieb_02871%29.jpg/220px-Ella_Fitzgerald_%28Gottlieb_02871%29.jpg',
         alt: 'Ella Fitzgerald',
-        bio: 'https://kids.britannica.com/kids/article/Ella-Fitzgerald/353131'
+        bio: 'https://kids.britannica.com/kids/article/Ella-Fitzgerald/353131',
+        quiz: 'fitzgeraldquiz'
     },
     {
         name: 'Samuel Coleridge-Taylor',
         image: 'https://www.blackpast.org/wp-content/uploads/prodimages/files/blackpast_images/Coleridge-Taylor_Samuel.jpg',
         alt: 'Samuel Coleridge-Taylor',
-        bio: 'https://wiki.kidzsearch.com/wiki/Samuel_Coleridge-Taylor'
+        bio: 'https://wiki.kidzsearch.com/wiki/Samuel_Coleridge-Taylor',
+        quiz: 'coleridgequiz'
     },
     {
         name: 'Nina Simone',
         image: 'https://static01.nyt.com/images/2015/06/21/arts/21SIMONE/21SIMONE-superJumbo.jpg',
         alt: 'Nina Simone',
-        bio: 'https://wiki.kidzsearch.com/wiki/Nina_Simone'
+        bio: 'https://wiki.kidzsearch.com/wiki/Nina_Simone',
+        quiz: 'simonequiz'
     },
     {
         name: 'Ray Charles',
         image: 'https://images.pulsewebcontent.com/photos/2018/06_Jun/800/RayCharles_6_15.jpg',
         alt: 'Ray Charles',
-        bio: 'https://kids.kiddle.co/Ray_Charles'
+        bio: 'https://kids.kiddle.co/Ray_Charles',
+        quiz: 'charlesquiz'
     },
     {
         name: 'Marian Anderson',
         image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Marian_Anderson.jpg/220px-Marian_Anderson.jpg',
         alt: 'Marian Anderson',
-        bio: 'https://kids.britannica.com/students/article/Marian-Anderson/272858'
+        bio: 'https://kids.britannica.com/students/article/Marian-Anderson/272858',
+        quiz: 'andersonquiz'
     },
     {
         name: 'Dizzy Gillespie',
         image: 'https://willismusic.com/wp-content/uploads/2018/02/dizzy_gillespie.jpg',
         alt: 'Dizzy Gillespie',
-        bio: 'https://kids.britannica.com/students/article/Dizzy-Gillespie/324613'
+        bio: 'https://kids.britannica.com/students/article/Dizzy-Gillespie/324613',
+        quiz: 'gillespiequiz'
     },
     {
         name: 'Billie Holiday',
         image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Billie_Holiday_1947_%28cropped%29.jpg/220px-Billie_Holiday_1947_%28cropped%29.jpg',
         alt: 'Billie Holiday',
-        bio: 'https://kids.kiddle.co/Billie_Holiday'
+        bio: 'https://kids.kiddle.co/Billie_Holiday',
+        quiz: 'holidayquiz'
     }
 ]
 export default quizCards;
