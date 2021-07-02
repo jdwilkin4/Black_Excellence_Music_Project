6773d968204520e7801217ce2052409c79a3faa6
Jessica Wilkins <jwilkin4@hotmail.com>
2021-07-02 00:13:07 -0700

    adding artists to quizzes page

--- src/quizzes/quiz-cards.js
+++ src/quizzes/quiz-cards.js
@@ -22,6 +22,30 @@ const quizCards = [
         image: 'https://static01.nyt.com/images/2015/06/21/arts/21SIMONE/21SIMONE-superJumbo.jpg',
         alt: 'Nina Simone',
         bio: 'https://wiki.kidzsearch.com/wiki/Nina_Simone'
+    },
+    {
+        name: 'Ray Charles',
+        image: 'https://images.pulsewebcontent.com/photos/2018/06_Jun/800/RayCharles_6_15.jpg',
+        alt: 'Ray Charles',
+        bio: 'https://kids.kiddle.co/Ray_Charles'
+    },
+    {
+        name: 'Marian Anderson',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Marian_Anderson.jpg/220px-Marian_Anderson.jpg',
+        alt: 'Marian Anderson',
+        bio: 'https://kids.britannica.com/students/article/Marian-Anderson/272858'
+    },
+    {
+        name: 'Dizzy Gillespie',
+        image: 'https://willismusic.com/wp-content/uploads/2018/02/dizzy_gillespie.jpg',
+        alt: 'Dizzy Gillespie',
+        bio: 'https://kids.britannica.com/students/article/Dizzy-Gillespie/324613'
+    },
+    {
+        name: 'Billie Holiday',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Billie_Holiday_1947_%28cropped%29.jpg/220px-Billie_Holiday_1947_%28cropped%29.jpg',
+        alt: 'Billie Holiday',
+        bio: 'https://kids.kiddle.co/Billie_Holiday'
     }
 ]
 export default quizCards;
