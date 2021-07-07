5ceb34aff1e6b925830ca0efbcabf53f508ee7c8
Jessica Wilkins <jwilkin4@hotmail.com>
2021-07-06 21:35:59 -0700

    fixing functionality for armstrong quiz

--- src/quizzes/ArmstrongQuiz.jsx
+++ src/quizzes/ArmstrongQuiz.jsx
@@ -1,37 +1,97 @@
 import { useState } from 'react';
 import quiz from './armstrong-quiz-questions';
 
+const btnStyles = { cursor: "pointer" };
 
+const StartQuiz = () => {
+    const [currQuestion, setCurrQuestion] = useState(0);
+    const [showQuizPage, setShowQuizPage] = useState(true);
+    const [userPoints, setUserPoints] = useState(0);
+    const [totalQuestionsCorrect, setTotalQuestionsCorrect] = useState(0);
+    const totalPossiblePoints = 100;
+
+    const nextQuestion = (isCorrect) => {
+        if (isCorrect) {
+            setUserPoints(userPoints + 10);
+            setTotalQuestionsCorrect(totalQuestionsCorrect + 1);
+        }
+        currQuestion === quiz.length - 1
+            ? setShowQuizPage(false)
+            : setCurrQuestion(currQuestion + 1);
+    };
+    return (
+        <>
+            {showQuizPage ? (
+                <div>
+                    <h2>{quiz[currQuestion].question}</h2>
+                    {quiz[currQuestion].answers.map((answer, index) => (
+                        <div key={index}>
+                            <button
+                                onClick={() => nextQuestion(answer.isCorrect)}
+                                style={btnStyles}
+                            >
+                                {answer.options}
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                    <ResultsPage
+                        totalPoints={totalPossiblePoints}
+                        points={userPoints}
+                        correctAnswers={totalQuestionsCorrect}
+                    />
+                )}
+        </>
+    );
+};
+
+const ResultsPage = (props) => {
+    const [playAgain, setPlayAgain] = useState(false);
+    const resetQuiz = () => setPlayAgain(true);
+    return (
+        <>
+            {playAgain ? (
+                <StartQuiz />
+            ) : (
+                    <>
+                        <h2>Results:</h2>
+                        <p>
+                            You got {props.correctAnswers} out of {quiz.length}
+                        </p>
+                        <p>
+                            You got {props.points} out of {props.totalPoints}
+                        </p>
+                        <button onClick={resetQuiz} style={btnStyles}>
+                            Take again?
+            </button>
+                    </>
+                )}
+        </>
+    );
+};
 
 const ArmstrongQuiz = () => {
-    //const [currentNum, setCurrentNum] = useState(0);
-    const currQuestion = quiz[0];
-    //const [userAnswer, setUserAnswer] = useState('');
 
-
-    const getValue = (e) => console.log(e.target.value);
+    const [showStarterPrompt, setShowStarterPrompt] = useState(true);
+    const displayQuiz = () => setShowStarterPrompt(false);
 
     return (
-        <div>
-            <h1>How well do you know Louis Armstrong?</h1>
-            <div>
-                <h2>{currQuestion.id} {currQuestion.question}</h2>
-                {currQuestion.answers.map((answer, index) => (
-                    <div key={index}>
-                        <input
-                            className='w-1/2 px-4 py-3 my-4 text-white bg-green-700  rounded-lg font-bold text-sm'
-                            type='button'
-                            value={answer}
-                            onChange={getValue}
-                            onClick={getValue}
-                        />
-                    </div>
-                ))}
-            </div>
+        <>
+            {showStarterPrompt ? (
+                <div>
+                    <h1>How well do you know Duke Ellington?</h1>
+                    <p>Test your skills</p>
+                    <button style={btnStyles} onClick={displayQuiz}>
+                        Start quiz
+            </button>
+                </div>
+            ) : (
+                    <StartQuiz />
+                )}
+        </>
+    );
 
-        </div>
-
-    )
 }
 
 
--- src/quizzes/armstrong-quiz-questions.js
+++ src/quizzes/armstrong-quiz-questions.js
@@ -2,52 +2,52 @@ const quiz = [
   {
     id: '1)',
     question: 'When was Louis Armstrong born?',
-    answers: ['July 15, 1907', 'August 4, 1901', 'May 22, 1900', 'September 1, 1902'],
-    correctAnswer: 'August 4, 1901'
+    answers: [
+      { options: 'July 15, 1907', isCorrect: false },
+      { options: 'August 4, 1901', isCorrect: true },
+      { options: 'May 22, 1900', isCorrect: false },
+      { options: 'September 1, 1902', isCorrect: false }
+    ]
   },
   {
-    id: 2,
+    id: '2)',
     question: 'Where was Louis Armstrong born?',
-    answers: {
-      a: 'Los Angeles,California',
-      b: 'New York City, New York',
-      c: 'Chicago, Illinois',
-      d: 'New Orleans, Louisiana',
-    },
-    correctAnswer: 'New Orleans, Louisiana'
+    answers: [
+      { options: 'Los Angeles,California', isCorrect: false },
+      { options: 'New York City, New York', isCorrect: false },
+      { options: 'Chicago, Illinois', isCorrect: false },
+      { options: 'New Orleans, Louisiana', isCorrect: true }
+    ]
   },
   {
-    id: 3,
+    id: '3)',
     question: 'When was Louis Armstrong\'s big break as a trumpet player?',
-    answers: {
-      a: 1922,
-      b: 1930,
-      c: 1917,
-      d: 1929,
-    },
-    correctAnswer: 1922
+    answers: [
+      { options: 1922, isCorrect: true },
+      { options: 1930, isCorrect: false },
+      { options: 1917, isCorrect: false },
+      { options: 1929, isCorrect: false }
+    ]
   },
   {
-    id: 4,
+    id: '4)',
     question: 'What year was Louis Armstrong’s All-Stars formed?',
-    answers: {
-      a: 1920,
-      b: 1930,
-      c: 1940,
-      d: 1950,
-    },
-    correctAnswer: 1940
+    answers: [
+      { options: 1920, isCorrect: false },
+      { options: 1930, isCorrect: false },
+      { options: 1940, isCorrect: true },
+      { options: 1950, isCorrect: false }
+    ]
   },
   {
-    id: 5,
+    id: '5)',
     question: 'When did Louis Armstrong die?',
-    answers: {
-      a: 'July 16, 1971',
-      b: 'July 6, 1971',
-      c: 'July 26, 1971',
-      d: 'July 8, 1971',
-    },
-    correctAnswer: 'July 6, 1971'
+    answers: [
+      { options: 'July 16, 1971', isCorrect: false },
+      { options: 'July 6, 1971', isCorrect: true },
+      { options: 'July 26, 1971', isCorrect: false },
+      { options: 'July 8, 1971', isCorrect: false }
+    ]
   },
 ]
 export default quiz;
