f2d81a5a33a63275b03dbeb07b71a611a09ad922
Jessica Wilkins <jessicawilkins@Dedrias-MacBook-Pro-2.local>
2021-07-19 03:52:26 -0700

    creating hero section for home page

--- src/misc-components/Home.jsx
+++ src/misc-components/Home.jsx
@@ -2,45 +2,31 @@ const Home = () => {
   return (
     <>
       <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
-        <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
-          <p class="uppercase tracking-loose w-full">What business are you?</p>
-          <h1 class="my-4 text-5xl font-bold leading-tight">
-            Main Hero Message to sell yourself!
+        <div class="flex flex-col w-full justify-center  text-center ">
+          <h1 class="my-4 text-6xl font-bold leading-tight">
+            Black Excellence Music Project
           </h1>
           <p class="leading-normal text-2xl mb-8">
-            Sub-hero message, not too long and not too short. Make it just right!
+            Learn. Explore. Celebrate.
           </p>
-          <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-            Subscribe
-          </button>
         </div>
-        <div class="w-full md:w-3/5 py-6 text-center">
-          <img class="w-full md:w-4/5 z-50" src="hero.png" />
+        <div class="flex-wrap w-full justify-center	  py-6 text-center flex -space-x-2">
+
+          <img class="inline-block h-60 w-60 rounded-full object-cover ring-2 ring-white" src="https://www.wrti.org/sites/wrti/files/styles/x_large/public/201703/EllaFitzgerald923px.jpg" alt="Ella Fitzgerald" />
+          <img class="inline-block h-60 w-60 rounded-full object-cover ring-2 ring-white" src="https://bloximages.newyork1.vip.townnews.com/theadvocate.com/content/tncms/assets/v3/editorial/7/b3/7b38444d-ce60-5ca9-b3b3-1235ec5498a7/57518c10ab93c.image.jpg?crop=743%2C743%2C0%2C123&resize=1200%2C1200&order=crop%2Cresize" alt="Terrance Blanchard" />
+          <img class="inline-block h-60 w-60 rounded-full object-cover ring-2 ring-white" src="https://cdn.michiganopera.org/20210303111250/Margaret-Bonds-722.jpg" alt="Margaret Bonds" />
+          <img class="inline-block h-60 w-60 rounded-full object-cover ring-2 ring-white" src="https://lcweb2.loc.gov/diglib/ihas/html/images/nathaniel-dett.jpg" alt="Robert Nathaniel Dett" />
+
         </div>
+
       </div>
 
-      {/*   <div class="pt-24">
 
-    </div>
-    <div class="relative -mt-12 lg:-mt-24">
-      <svg viewBox="0 0 1428 174" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-          <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fill-rule="nonzero">
-            <path d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496" opacity="0.100000001"></path>
-            <path
-              d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
-              opacity="0.100000001"
-            ></path>
-            <path d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z" id="Path-4" opacity="0.200000003"></path>
-          </g>
-          <g transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fill-rule="nonzero">
-            <path
-              d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"
-            ></path>
-          </g>
-        </g>
-      </svg>
-    </div>
+
+      {/*   
+
+  
+   
     <section class="bg-white border-b py-8">
       <div class="container max-w-5xl mx-auto m-8">
         <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">
