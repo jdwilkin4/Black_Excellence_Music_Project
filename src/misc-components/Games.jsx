7b2b220490ab4b272558da5605f5427275012f38
Jessica Wilkins <jessicawilkins@Dedrias-MacBook-Pro-2.local>
2021-07-26 01:32:52 -0700

    finishing game page

--- src/misc-components/Games.jsx
+++ src/misc-components/Games.jsx
@@ -1,8 +1,57 @@
+import { Link } from "react-router-dom";
+
+
 const Games = () => {
     return (
         <>
-            <p>game page</p>
+            <h1 className="text-5xl text-center my-6">Games</h1>
+            <GamesTemplate
+                text={'Experience what is was like to be a young black jazz pianist in 1930\'s America.'}
+                path='/dayinlife'
+                linkName='Day in the Life'
+                img='https://i.imgur.com/epWr8mg.jpg'
+                alt='New York City'
+                gameCategory='Day in the Life Game'
+            />
+            <GamesTemplate
+                text='How well do you know these artists?'
+                path='/quizzes'
+                linkName='Take a quiz'
+                img='https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Louis_Armstrong_restored.jpg/220px-Louis_Armstrong_restored.jpg'
+                alt='Louis Armstrong'
+                gameCategory='Quizzes'
+            />
+
         </>
     )
 }
 export default Games;
+
+
+//games template
+
+const GamesTemplate = (props) => {
+    return (
+        <div className="mx-auto px-4 py-8 max-w-xl my-10">
+            <div className="bg-white shadow-2xl rounded-lg  tracking-wide" >
+                <div className="md:flex-shrink-0">
+                    <img src={props.img} alt={props.alt} className="w-full h-64 rounded-lg rounded-b-none" />
+                </div>
+                <div className="px-4 py-2 mt-2">
+                    <h2 className="font-bold text-center text-2xl text-black-800 tracking-normal">{props.gameCategory}</h2>
+                </div>
+
+                <p className=" pt-4 text-xl text-center"> {props.text} </p>
+                <div className=" text-center mt-4 mb-8">
+                    <Link
+                        className=" px-4 py-2 font-bold text-white bg-purple-700 rounded-full focus:outline-none focus:shadow-outline"
+                        to={props.path}>
+                        {props.linkName}
+                    </Link>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
