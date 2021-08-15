f8eab27bf04e52d6baf15ff6c28865727f3a32ba
Jessica Wilkins <jessicawilkins@Dedrias-MacBook-Pro-2.local>
2021-08-15 14:53:51 -0700

    adding tests for loading page

---
+++ src/misc-components/Loading.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Loading from './Loading';
+
+test('renders loading message', () => {
+    render(<Loading />)
+    expect(screen.getByText(/Loading.../)).toBeInTheDocument()
+});
+
+test('renders page will load soon', () => {
+    render(<Loading />)
+    expect(screen.getByText(/Your page will load soon./)).toBeInTheDocument()
+});
