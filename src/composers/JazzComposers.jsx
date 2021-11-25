6f7b571f769d6f856ae474fa218687e0182e1603
jdwilkin4 <jwilkin4@hotmail.com>
2021-11-24 20:55:23 -0800

    refactoring jazz composers page

--- src/composers/JazzComposers.jsx
+++ src/composers/JazzComposers.jsx
@@ -1,39 +1,59 @@
-import React, { useState } from 'react';
-import AllJazzComposers from './AllJazzComposers';
-import FemaleJazzComposers from './FemaleJazzComposers';
-import MaleJazzComposers from './MaleJazzComposers';
+import React, { useState, lazy, Suspense } from 'react';
+import jazzComposersData from '../artists-data/jazz-composers';
+import Loading from '../misc-components/Loading';
+const ArtistTemplate = lazy(() => import('../misc-components/ArtistTemplate'));
+const renderLoader = () => <Loading />;
+const femaleComposerArr = jazzComposersData.filter(composer => composer.isFemale)
 
-const ChangeCategory = (props) => {
-    if (props.category === 'all') {
-        return <AllJazzComposers />
-    } else if (props.category === 'female') {
-        return <FemaleJazzComposers />
-    } else if (props.category === 'male') {
-        return <MaleJazzComposers />
-    }
+
+const ChangeCategory = ({ category }) => {
+  return (
+    <Suspense fallback={renderLoader()}>
+      {category === "all" ? (
+        <>
+          <ArtistTemplate
+            arr={jazzComposersData}
+            borderColor="border-purple-700"
+            bgColor="bg-purple-700"
+          />
+        </>)
+        : (
+          <>
+            <ArtistTemplate
+              arr={femaleComposerArr}
+              borderColor="border-green-700"
+              bgColor="bg-green-700"
+            />
+          </>
+        )}
+    </Suspense>
+
+  )
+
 }
 const JazzComposersPage = () => {
-    const [option, setOption] = useState('all');
-    const handleChange = (e) => setOption(e.target.value);
+  const [option, setOption] = useState('all');
+  const handleChange = (e) => setOption(e.target.value);
+  const values = ["all", "female"];
 
-    return (
-        <div>
-            <h1 className="text-5xl text-center my-8 ">Jazz Composers</h1>
-            <div className="container mx-auto flex justify-center items-center p-2 md:p-0">
-                <div className="border border-purple-900 p-6 grid grid-cols-1 gap-6 bg-white shadow-lg rounded-lg">
-                    <h2 className="text-2xl text-center my-2">Filter by category</h2>
-                    <select onChange={handleChange} value={option} className="border border-purple-300 p-2 rounded">
-                        <option value="all">All Jazz Composers</option>
-                        <option value="female">Female Composers</option>
-                        <option value="male">Male Composers</option>
-                    </select>
-                </div>
-            </div>
-
-            <ChangeCategory category={option} />
-
-
+  return (
+    <div>
+      <h1 className="text-5xl text-center my-8 ">Jazz Composers</h1>
+      <div className="container mx-auto flex justify-center items-center p-2 md:p-0">
+        <div className="border border-purple-900 p-6 grid grid-cols-1 gap-6 bg-white shadow-lg rounded-lg">
+          <h2 className="text-2xl text-center my-2">Filter by category</h2>
+          <select onChange={handleChange} value={option} className="border border-purple-300 p-2 rounded">
+            {values.map((val, index) => (
+              <option key={index} value={val}>{val.toUpperCase()}</option>
+            ))}
+          </select>
         </div>
-    )
+      </div>
+
+      <ChangeCategory category={option} />
+
+
+    </div>
+  )
 }
-export default JazzComposersPage
+export default JazzComposersPage;
--- src/composers/JazzComposers.test.js
+++ src/composers/JazzComposers.test.js
@@ -4,15 +4,11 @@ import JazzComposers from './JazzComposers';
 
 test('renders All Jazz Composers', () => {
     render(<JazzComposers />)
-    expect(screen.getByText(/All Jazz Composers/)).toBeInTheDocument()
+    expect(screen.getByText(/ALL/)).toBeInTheDocument()
 });
 
 test('renders Female Composers', () => {
     render(<JazzComposers />)
-    expect(screen.getByText(/Female Composers/)).toBeInTheDocument()
+    expect(screen.getByText(/FEMALE/)).toBeInTheDocument()
 });
 
-test('renders Male Composers', () => {
-    render(<JazzComposers />)
-    expect(screen.getByText(/Male Composers/)).toBeInTheDocument()
-});
