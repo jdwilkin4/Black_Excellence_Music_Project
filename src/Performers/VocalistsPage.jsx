ae370f326e76f10404bd60af3afd9831be1da5d3
jdwilkin4 <jwilkin4@hotmail.com>
2021-12-04 20:23:27 -0800

    refactoring vocal page

--- src/Performers/VocalistsPage.jsx
+++ src/Performers/VocalistsPage.jsx
@@ -1,39 +1,55 @@
-import React, { useState } from 'react';
-import AllVocalists from './AllVocalists';
-import JazzVocalists from './JazzVocalists';
-import ClassicalVocalists from './ClassicalVocalists';
+import React, { useState, lazy, Suspense } from 'react';
+import vocalists from '../artists-data/vocalists';
+import Loading from '../misc-components/Loading';
+const ArtistTemplate = lazy(() => import('../misc-components/ArtistTemplate'));
+const renderLoader = () => <Loading />
+
 
 const VocalistsPage = () => {
-    const [option, setOption] = useState('all');
-    const handleChange = (e) => setOption(e.target.value);
+  const [option, setOption] = useState('all');
+  const handleChange = (e) => setOption(e.target.value);
+  const valuesArr = ['all', 'jazz', 'classical'];
+  const classicalVocalArr = vocalists.filter(musician => musician.category === 'classical');
+  const jazzVocalArr = vocalists.filter(musician => musician.category === 'jazz');
 
-    const ChangeCategory = (props) => {
-        if (props.category === 'all') {
-            return <AllVocalists />
-        } else if (props.category === 'jazz') {
-            return <JazzVocalists />
-        } else if (props.category === 'classical') {
-            return <ClassicalVocalists />
-        }
+
+  const categoryObj = {
+    "all": vocalists,
+    "classical": classicalVocalArr,
+    "jazz": jazzVocalArr
+  }
+
+  const ChangeCategory = ({ category }) => {
+    if (categoryObj.hasOwnProperty(category)) {
+      return (
+        <Suspense fallback={renderLoader()}>
+          <ArtistTemplate
+            arr={categoryObj[category]}
+            borderColor="border-green-700"
+            bgColor="bg-green-700"
+          />
+        </Suspense>
+      )
     }
+  }
 
-    return (
-        <div>
-            <h1 className="text-5xl text-center my-8 ">Vocalists</h1>
-            <div className="container mx-auto flex justify-center items-center p-2 md:p-0">
-                <div className="border border-purple-900 p-6 grid grid-cols-1 gap-6 bg-white shadow-lg rounded-lg">
-                    <h2 className="text-2xl text-center my-2">Filter by category</h2>
-                    <select onChange={handleChange} value={option} className="border border-purple-300 p-2 rounded">
-                        <option value="all">All</option>
-                        <option value="jazz">Jazz Vocalists</option>
-                        <option value="classical">Classical Vocalists</option>
-                    </select>
-                </div>
-            </div>
-            <ChangeCategory category={option} />
-
+  return (
+    <div>
+      <h1 className="text-5xl text-center my-8 ">Vocalists</h1>
+      <div className="container mx-auto flex justify-center items-center p-2 md:p-0">
+        <div className="border border-purple-900 p-6 grid grid-cols-1 gap-6 bg-white shadow-lg rounded-lg">
+          <h2 className="text-2xl text-center my-2">Filter by category</h2>
+          <select onChange={handleChange} value={option} className="border border-purple-300 p-2 rounded">
+            {valuesArr.map((val, index) => (
+              <option value={val} key={index}>{val.toUpperCase()}</option>
+            ))}
+          </select>
         </div>
+      </div>
+      <ChangeCategory category={option} />
 
-    )
+    </div>
+
+  )
 }
 export default VocalistsPage;
--- src/Performers/VocalistsPage.test.js
+++ src/Performers/VocalistsPage.test.js
@@ -4,15 +4,15 @@ import VocalistsPage from './VocalistsPage';
 
 test('renders All', () => {
     render(<VocalistsPage />)
-    expect(screen.getByText(/All/)).toBeInTheDocument()
+    expect(screen.getByText(/ALL/)).toBeInTheDocument()
 });
 
 test('renders Jazz Vocalists', () => {
     render(<VocalistsPage />)
-    expect(screen.getByText(/Jazz Vocalists/)).toBeInTheDocument()
+    expect(screen.getByText(/JAZZ/)).toBeInTheDocument()
 });
 
 test('renders Classical Vocalists', () => {
     render(<VocalistsPage />)
-    expect(screen.getByText(/Classical Vocalists/)).toBeInTheDocument()
+    expect(screen.getByText(/CLASSICAL/)).toBeInTheDocument()
 });
